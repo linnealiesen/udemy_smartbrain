ce23d8f0fc4293d796360892c546c75339faa028
Linnea Liesén <linnealiesen@protonmail.com>
2022-01-06 15:33:32 +0100

    initial commit

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -6,11 +6,9 @@ import './Logo.css';
 const Logo = () => {
 	return (
 		<div className='ma4 mt0'>
-			<Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
-				<div className="Tilt-inner pa3"> 
-					<img style={{paddingTop:'5px'}} alt='logo' src={brainpng}/> 
-				</div>
-			</Tilt>
+			 <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: '150px', width: '150px' }} >
+	      		<div className="Tilt inner pa3 center"> <img style={{paddingTop: '5px'}} src={brainpng} alt='logo'/> </div>
+	    	</Tilt>
 		</div>
 	);
 }
